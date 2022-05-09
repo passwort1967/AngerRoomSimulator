f6de6e51ef09513106f5843216e340497afdd22d
Philippe <philippe.heubueschl@gmail.com>
2022-05-09 17:43:15 +0200

    Added Hand Animation
    Not perfect

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    public float speed;
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        AnimateHand();
+        
+    }
+
+    internal void SetGrip(float readValue)
+    {
+        gripTarget = readValue;
+    }
+
+    internal void SetTrigger(float readValue)
+    {
+        triggerTarget = readValue;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        }
+
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorTriggerParam, gripCurrent);
+        }
+    }
+}
